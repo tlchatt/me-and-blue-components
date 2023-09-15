90a34cbf0e0885c513af193983e63db4fb5e00c9
dreadpirate007 <admin@tlchatt.com>
2023-09-15 15:10:22 -0400

    dev commits

--- app/page.js
+++ app/page.js
@@ -1,25 +1,21 @@
 'use client'
 import { createPortal } from 'react-dom';
 import React, { useState, useEffect } from 'react';
-
+import { Contact } from './ContactForm';
 export default function Page({ section, appearance, branding, handleContactToggle, contactOpen }) {
-    console.log('Page')
     const [contactForm, setContactForm] = useState(null);
     useEffect(() => {
         console.log('useEffect')
         var contactForm = document.querySelector ('#technologic-contact'); 
-        console.log('contactForm' ,contactForm)
+        console.log('contactForm' ,)
         if (contactForm){
             console.log('if (contactForm){')
             setContactForm(contactForm)
         }
       }, []);
 
-      return(<p>test {console.log('test')}</p>)
-      {contactForm !== null && createPortal(
-        <p>Hello from React!</p>,
-        contactForm
-      )}
+      return(<Contact/>)
+
 
 
 
