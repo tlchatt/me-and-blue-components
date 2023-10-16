d48c0076d83719b217d933ff5f4e06cb91e2e19b
dreadpirate007 <admin@tlchatt.com>
2023-10-16 16:40:34 -0400

    testing content type header

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
+  async headers() {
+    return [
+      {
+        source: '/ContactForm2',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript',
+          }
+        ],
+      },
+    ]
+  }
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
