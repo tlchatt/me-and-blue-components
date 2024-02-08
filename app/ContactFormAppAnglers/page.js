c5c39a9fed8f6acbf64bb15dea7ab493a186645f
dreadpirate007 <admin@tlchatt.com>
2024-02-08 17:25:40 -0500

    fixes

--- app/ContactFormAppAnglers/page.js
+++ app/ContactFormAppAnglers/page.js
@@ -77,16 +77,43 @@ let contactSection = {
   },
   "notes": "Specify Time Zone in this format https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
 }
+  /**
+   * Implementation as component in another site.
+   * <iframe src="https://component.tlchatt.com/ContactFormAppAnglers" style="height: 700px; width:
+      100%;border: 0;" id="technologic-contact"></iframe>
+      <script>
+
+        window.addEventListener("message", handleMessage);
+
+        function handleMessage(event) {
+          if(event.origin != 'https://appalachiananglers.com' ){
+              console.log("Received a message from " + event.origin + ".");
+            if (event.data.type == 'Contact_Submit'){
+              gtag('event', 'Contact_Submit');
+            }
+          // When one window sends a message, or data, to another window via
+          // `parent.postMessage()`, the message (the first argument) in the
+          // `parent.postMessage()` call is accessible via `event.data` here.
+          // Do something with the data.
+          console.log(event.data);
+          }
+
+        }
+      </script>
+   * 
+   */
 export default function Page() {
   return (
       <GlobalContextProvider>
       
         <Form section={contactSection} appearance={appearance} branding={branding}  />
 
-          <FooterNav appearance={appearance}
-        branding={branding} />
       </GlobalContextProvider>
   )
 
-
+/**
+ * 
+          <FooterNav appearance={appearance}
+        branding={branding} />
+ */
 }
