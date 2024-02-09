d957109fc98a949339ec10e0187f02a7ff5d32a5
dreadpirate007 <admin@tlchatt.com>
2024-02-09 15:07:18 -0500

    dev

--- public/Contact.js
+++ public/Contact.js
@@ -3,7 +3,23 @@
           // `parent.postMessage()` call is accessible via `event.data` here.
           // Do something with the data.
           window.addEventListener("message", handleMessage);
-
+          let iframe
+          iframe = document.createElement('iframe');
+          iframe.title = 'Technologic Contact For Johnsen Diamond';
+          iframe.frameBorder = '0';
+          iframe.style.border = '0';
+          iframe.style.position = 'fixed';
+          iframe.style.bottom = '0';
+          iframe.style.right = '0';
+          iframe.style.zIndex = '1';
+          iframe.style.height = 'auto';
+          iframe.style.width = 'auto';
+          iframe.style. minWidth = '350px';
+          iframe.style.maxHeight = '100px';
+          iframe.src = 'https://component.tlchatt.com/ContactJohnsenDiamond';
+          iframe.id = 'Technologic_Iframe_Contact';
+          let body = document.querySelector('body');
+          body.appendChild(iframe);
           function handleMessage(event) {
             if(event.origin == 'https://component.tlchatt.com.' ){
                 console.log("Received a message from " + event.origin + ".");
